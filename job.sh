e874b6926c7a147685185d6a2438e9bd69c5a467
Kingsley Nweye <etonwana@yahoo.com>
2023-07-25 14:12:30 -0500

    added jobs

---
+++ job.sh
@@ -0,0 +1,8 @@
+python simulate.py train sac parisa_ev_with_all_building_solar 20 CustomEVReward models results
+python simulate.py train sac parisa_ev_with_all_building_solar 20 MARL models results
+python simulate.py train marlisa parisa_ev_with_all_building_solar 20 CustomEVReward models results
+python simulate.py train marlisa parisa_ev_with_all_building_solar 20 MARL models results
+python simulate.py train sac parisa_ev_without_all_building_solar 20 CustomEVReward models results
+python simulate.py train sac parisa_ev_without_all_building_solar 20 MARL models results
+python simulate.py train marlisa parisa_ev_without_all_building_solar 20 CustomEVReward models results
+python simulate.py train marlisa parisa_ev_without_all_building_solar 20 MARL models results
