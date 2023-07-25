b63f539e88880a07f04040c98ff3e9a47c37fca9
Kingsley Nweye <etonwana@yahoo.com>
2023-07-25 10:21:02 -0500

    added save model callback

--- citylearn/callback.py
+++ citylearn/callback.py
@@ -1,14 +1,34 @@
 import os
+import pickle
 import pandas as pd
 from stable_baselines3.common.callbacks import BaseCallback
 
+class SaveModelCallback(BaseCallback):
+    def __init__(self, model, simulation_id, simulation_output_path):
+        self.model = model
+        self.simulation_id = simulation_id
+        self.simulation_output_path = simulation_output_path
+        os.makedirs(simulation_output_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.model.env.time_step == self.model.env.time_steps - 2:
+            with open(os.path.join(
+                self.simulation_output_path, 
+                f'{self.simulation_id}_{self.model.env.time_step}_time_steps.pkl'), 'wb'
+            ) as f:
+                pickle.dump(self.model, f)
+            return True
+        
+        else:
+            return False
+        
 class SaveDataCallback(BaseCallback):
     """
     A custom callback that derives from ``BaseCallback``.
 
     :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
     """
-    def __init__(self, env, simulation_id, simulation_output_path, extras=None, verbose=0):
+    def __init__(self, env, simulation_id, simulation_output_path, extras=None, ignore_time_step=None, verbose=0):
         super(SaveDataCallback, self).__init__(verbose)
         self.env = env
         self.simulation_id = simulation_id
@@ -16,11 +36,12 @@ class SaveDataCallback(BaseCallback):
         self.episode = 0
         self.extras = {} if extras is None else extras
         self.mode = 'train'
+        self.ignore_time_step = False if ignore_time_step is None else ignore_time_step
         os.makedirs(simulation_output_path, exist_ok=True)
 
     def _on_step(self) -> bool:
         # save timer data
-        if self.env.time_step == self.env.time_steps - 2:
+        if self.env.time_step == self.env.time_steps - 2 or self.ignore_time_step:
             save_data(
                 self.env, 
                 self.simulation_id, 
@@ -38,9 +59,16 @@ class SaveDataCallback(BaseCallback):
     
 def save_data(env, simulation_id, simulation_output_path, episode, mode, extras):    
     # save reward data
-    reward_data = pd.DataFrame(env.rewards, columns=['reward'])
-    reward_data['time_step'] = reward_data.index
-    reward_data['building_name'] = None
+    if env.central_agent:
+        reward_data = pd.DataFrame(env.rewards, columns=['reward'])
+        reward_data['time_step'] = reward_data.index
+        reward_data['building_name'] = None
+    else:
+        building_names = [b.name for b in env.buildings]
+        reward_data = pd.DataFrame(env.rewards, columns=building_names)
+        reward_data['time_step'] = reward_data.index
+        reward_data = reward_data.melt(id_vars=['time_step'], value_vars=building_names, var_name='building_name', value_name='reward')
+
     reward_data['mode'] = mode
     reward_data['episode'] = episode
     reward_data['simulation_id'] = simulation_id
@@ -70,7 +98,7 @@ def save_data(env, simulation_id, simulation_output_path, episode, mode, extras)
     for k, v in extras.items():
         kpi_data[k] = v
 
-    kpi_filepath = os.path.join(simulation_output_path, f'kpis-{simulation_id}.csv')
+    kpi_filepath = os.path.join(simulation_output_path, f'kpi-{simulation_id}.csv')
 
     if episode > 0 and os.path.isfile(kpi_filepath):
         existing_data = pd.read_csv(kpi_filepath)
@@ -79,6 +107,5 @@ def save_data(env, simulation_id, simulation_output_path, episode, mode, extras)
     else:
         pass
  
-    kpi_data = kpi_data.dropna()
     kpi_data.to_csv(kpi_filepath, index=False)
     del kpi_data
